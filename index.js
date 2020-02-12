23017273789ae48eb0f39e85dcd6d0fa326aed49
igor <embiggenerd@gmail.com>
2020-02-11 22:45:15 -0500

    post tested

--- index.js
+++ index.js
@@ -24,8 +24,31 @@ server.get('/', (req, res) => {
   `);
 });
 
-server.post('/api/posts', (req, res, next) => {
+server.post('/api/posts', async (req, res, next) => {
+    try {
+        const { title, contents } = req.body
+        if (!title || !contents) {
+            const noTitleOrContents = new Error('Please provide title and contents for the post.')
+            noTitleOrContents.httpStatusCode = 400
+            throw noTitleOrContents
+        }
+        const { id } = await insert(req.body)
 
+        const newPost = await findById(id)
+        res.status(201).json(newPost)
+
+    } catch (e) {
+        if (e.httpStatusCode !== 500) {
+            return next(e)
+        }
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.httpStatusCode || 500).json({
+        errorMessage: err.message
+    })
 })
 
 // add an endpoint that returns all the messages for a hub
